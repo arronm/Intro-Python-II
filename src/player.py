3157619627d5bff511757a812d53b338ef06ef41
Arron M <marshall.arron@gmail.com>
2019-09-04 09:35:10 -0700

    Add initial Player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    """A player class."""
+
+    def __init__(self, room):
+        self.room = room
